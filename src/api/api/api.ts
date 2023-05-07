b8773505c844d8d1883c5ec19409330f091d34ee
Christian Clausen <chriss.clausen@googlemail.com>
2023-05-07 13:37:47 +0200

    Generate API

---
+++ src/api/api/api.ts
@@ -0,0 +1,7 @@
+export * from './achievementController.service';
+import { AchievementControllerService } from './achievementController.service';
+export * from './personController.service';
+import { PersonControllerService } from './personController.service';
+export * from './presenceController.service';
+import { PresenceControllerService } from './presenceController.service';
+export const APIS = [AchievementControllerService, PersonControllerService, PresenceControllerService];
